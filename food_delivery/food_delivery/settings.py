0d026bb7674f4db763f8e992129c049a692c7bab
DHRUVIL18 <dhruvil18022001@gmail.com>
2024-06-13 22:59:12 -0400

    Test commit

--- food_delivery/food_delivery/settings.py
+++ food_delivery/food_delivery/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+# test commit
 from datetime import timedelta
 from pathlib import Path
 
